1c83fa182ac3c7a20122221eb81766307800e274
Pushkar-IITJ <m22aie213@iitj.ac.in>
2023-09-07 12:32:19 +0530

    first_commit

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -27,29 +27,4 @@ X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(data, digi
 # Learn the digits on the train subset
 clf.fit(X_train, y_train)
 
-# Predict the value of the digit on the test subset
-disp = predict_and_eval(clf, X_test, y_test)
-
-###############################################################################
-# If the results from evaluating a classifier are stored in the form of a
-# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
-# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
-# as follows:
-
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
+predict_and_eval(clf,X_test,y_test)
