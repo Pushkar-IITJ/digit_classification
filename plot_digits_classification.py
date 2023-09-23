92f7879a7869091cbf4972ff8a2c00f5a5b9479a
Pushkar-IITJ <m22aie213@iitj.ac.in>
2023-09-23 14:08:01 +0530

    quiz 1 q1 changes

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -14,7 +14,7 @@ from utils import split_train_dev_test, predict_and_eval, read_data, model_train
 X, y = read_data()
 
 # Split data into 70% train, 15% dev and 15% test subsets
-X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(X, y, test_size=0.15, dev_size=0.15)
+# X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(X, y, test_size=0.15, dev_size=0.15)
 
 # # Create a classifier: a support vector classifier
 # clf = model_training(X_train,y_train,{'gamma':0.001},model='svm')
@@ -24,14 +24,14 @@ X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(X, y, test
 
 # assignment 3: Task 2
 # Hyper parameter all possible combinations
-list_of_all_param_combination=param_combination(model='svm')
+# list_of_all_param_combination=param_combination(model='svm')
 
 # Best parameters, model and accuracy after Hyper parameter tuning
-best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination)
+# best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination)
 
 # assignment 3: Task 3 and 4 
-test_dev_variations(X,y)
+# test_dev_variations(X,y)
 
 # print
 print(f"The number of total samples in the dataset: {len(y)}")
-print(f"Size (height and width) of the images in dataset: {len(X),len(y)}")
+print(f"Size (height and width) of the images in dataset: {len(y),len(X[0])}")
