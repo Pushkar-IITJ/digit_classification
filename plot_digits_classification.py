5bd6e9a750ee3f8a034867afb807b50d218a638e
Pushkar-IITJ <m22aie213@iitj.ac.in>
2023-10-21 14:07:19 +0530

    ml_ops_quiz2

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -7,8 +7,9 @@ This code shows how scikit-learn can be used to recognize images of
 hand-written digits, from 0-9.
 
 """
-
+import numpy as np
 from sklearn import  metrics, svm
+from sklearn.tree import DecisionTreeClassifier
 from utils import split_train_dev_test, predict_and_eval, read_data, model_training, param_combination, tune_hparams, test_dev_variations
 
 X, y = read_data()
@@ -25,13 +26,45 @@ X_train, y_train, X_dev, y_dev, X_test, y_test = split_train_dev_test(X, y, test
 # assignment 3: Task 2
 # Hyper parameter all possible combinations
 list_of_all_param_combination=param_combination(model='svm')
-
-# Best parameters, model and accuracy after Hyper parameter tuning
 best_hparams, best_model, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination)
+production_model = model_training(X_train, y_train, best_hparams, model='svm')
+production_accuracy = predict_and_eval(production_model, X_test, y_test)
 
-# assignment 3: Task 3 and 4 
-test_dev_variations(X,y)
+candidate_hparams = {'max_depth': 10, 'min_samples_split': 2}
+candidate_model = DecisionTreeClassifier(**candidate_hparams)
+candidate_model.fit(X_train, y_train)
+candidate_accuracy = predict_and_eval(candidate_model, X_test, y_test)
 
-# print
-print(f"The number of total samples in the dataset: {len(y)}")
-print(f"Size (height and width) of the images in dataset: {len(X),len(y)}")
+
+production_predictions = production_model.predict(X_test)
+candidate_predictions = candidate_model.predict(X_test)
+
+# confusion_matrix = metrics.confusion_matrix(y_test, production_predictions)
+confusion_matrix = metrics.confusion_matrix(production_predictions, candidate_predictions)
+
+true_positives_production = np.logical_and(production_predictions == y_test, candidate_predictions == production_predictions).sum()
+
+false_negatives_production = np.logical_and(production_predictions == y_test, candidate_predictions != production_predictions).sum()
+
+false_positives_production = np.logical_and(candidate_predictions == y_test, candidate_predictions != production_predictions).sum()
+
+true_negatives_production = np.logical_and(candidate_predictions != y_test, candidate_predictions == production_predictions).sum()
+
+
+confusion_matrix_2x2 = np.array([[true_positives_production, false_negatives_production],
+                                  [false_positives_production, true_negatives_production]])
+
+# Production Macro-average F1 score
+production_macro_avg_f1 = metrics.f1_score(y_test, production_predictions, average='macro')
+
+# Candidate Macro-average F1 score
+candidate_macro_avg_f1 = metrics.f1_score(y_test, candidate_predictions, average='macro')
+
+print(f"Production model's accuracy: {production_accuracy}")
+print(f"Candidate model's accuracy: {candidate_accuracy}")
+print("Confusion Matrix:")
+print(confusion_matrix)
+print("confusion_matrix_2x2:")
+print(confusion_matrix_2x2)
+print(f"Macro-average F1 score: {production_macro_avg_f1}")
+print(f"Macro-average F1 score: {candidate_macro_avg_f1}")
